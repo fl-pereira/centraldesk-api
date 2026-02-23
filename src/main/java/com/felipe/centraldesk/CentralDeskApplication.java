05e50b06f4a6db3eb7a9a0a086727d19e0408fe7
Felipe Luiz Pereira <fpereira.media@gmail.com>
2026-02-23 19:49:57 -0300

    Repositório iniciado - Setup Spring Boot

---
+++ src/main/java/com/felipe/centraldesk/CentralDeskApplication.java
@@ -0,0 +1,13 @@
+package com.felipe.centraldesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralDeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipe/centraldesk/CentralDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.centraldesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
